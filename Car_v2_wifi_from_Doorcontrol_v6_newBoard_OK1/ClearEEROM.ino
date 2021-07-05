caeea22705a34de2e3dd58650c0ba99c4fffe904
dogbor <86064533+dogbor@users.noreply.github.com>
2021-07-06 00:20:02 +0700

    Add files via upload

---
+++ Car_v2_wifi_from_Doorcontrol_v6_newBoard_OK1/ClearEEROM.ino
@@ -0,0 +1,11 @@
+
+
+void ClearEEROM_setup() {
+
+// EEPROM Clear
+  EEPROM.begin(512);
+  // write a 0 to all 512 bytes of the EEPROM
+  for (int i = 0; i < 512; i++) {
+    EEPROM.write(i, 0);
+  }
+}
